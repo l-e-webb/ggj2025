1b6994c5d2047199b68fdbb49ce309959df54e4b
estherfraser <estheramfraser@gmail.com>
2025-01-25 12:09:21 -0400

    bubbles launch

--- bubble_wand_controller.gd
+++ bubble_wand_controller.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 
 var bubble_radius = 0.1
+var bubble = null
+const PLAIN_BUBBLE = preload("res://bubbles/PlainBubble.tscn")
+@export var scale_factor = 0.1
 
 # movement
 func _process(delta: float) -> void:
@@ -10,19 +13,38 @@ func _process(delta: float) -> void:
 	
 	position.x = mouse_x
 	
+	if bubble != null and bubble.scale.x < Constants.MAX_BUBBLE_SCALE:
+		bubble.scale += delta * scale_factor * Vector2(1, 1)
+		print(bubble.scale)
+	
 
 # on click create bubble/on release finalize bubble
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("bubble_start"):
 		blow_bubble()
 	if event.is_action_released("bubble_start"):
-		print("bubble released")
-		bubble_radius = 0
+		if bubble != null:
+			release_bubble()
+			print("bubble released")
+		else:
+			print("no bubble")
+			
 		
 func blow_bubble():
-	while bubble_radius < Constants.MAX_BUBBLE_RADIUS:
-		bubble_radius += 0.1
-		print(bubble_radius)
+	bubble = PLAIN_BUBBLE.instantiate()
+	add_child(bubble)
+	bubble.scale = Vector2(0.1, 0.1)
+	
+	
+func release_bubble():
+	bubble.is_floating = true
+	var bubble_global_pos = bubble.global_position
+	remove_child(bubble)
+	
+	get_tree().root.add_child(bubble)
+	bubble.global_position = bubble_global_pos
+	
+	bubble = null
 	
 
 # stretch: change bubble type
--- contants.gd
+++ contants.gd
@@ -1,3 +1,3 @@
 class_name Constants extends Node
 
-static var MAX_BUBBLE_RADIUS = 0.9999
+static var MAX_BUBBLE_SCALE = 0.9999
