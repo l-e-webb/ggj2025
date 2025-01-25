b4a1ed9c12421ab5e6c36ef3098ed18e370bc55d
estherfraser <estheramfraser@gmail.com>
2025-01-25 12:09:21 -0400

    bubbles launch

--- bubbles/plain_bubble_controller.gd
+++ bubbles/plain_bubble_controller.gd
@@ -7,11 +7,14 @@ extends CharacterBody2D
 @export var bubble_drift_frequency: float = 1.5
 
 var t: float = 0
+var is_floating = false
 
 func _physics_process(delta: float) -> void:
-	t += delta
-	t = fmod(t, TAU/bubble_drift_frequency)
-	velocity.x = cos(t * bubble_drift_frequency) * bubble_drift_amplitude
-	velocity.y = maxf(velocity.y - rise_accel * delta, -rise_max_speed)
+	
+	if is_floating:
+		t += delta
+		t = fmod(t, TAU/bubble_drift_frequency)
+		velocity.x = cos(t * bubble_drift_frequency) * bubble_drift_amplitude
+		velocity.y = maxf(velocity.y - rise_accel * delta, -rise_max_speed)
 
-	move_and_collide(velocity)
+		move_and_collide(velocity)
