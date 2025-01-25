7e557fbfb2259267b14bdfa5b8ac498ad49e451f
Louis Webb <louis.webb5@gmail.com>
2025-01-25 13:31:20 -0400

    Reorg and improve game scene

--- bubbles/plain_bubble_controller.gd
+++ bubbles/plain_bubble_controller.gd
@@ -9,17 +9,26 @@ extends AnimatableBody2D
 var velocity: Vector2 = Vector2(0,0)
 var t: float = 0
 var is_floating = false
+var is_pop_timer_set = false
 
 func _physics_process(delta: float) -> void:
-	
 	if is_floating:
+		if not is_pop_timer_set:
+			set_pop_timer()
+		
 		t += delta
 		t = fmod(t, TAU/bubble_drift_frequency)
 		velocity.x = cos(t * bubble_drift_frequency) * bubble_drift_amplitude
 		velocity.y = maxf(velocity.y - rise_accel * delta, -rise_max_speed)
 
 		var collision = move_and_collide(velocity)
-		
+
+func set_pop_timer():
+	get_tree().create_timer(
+		Constants.MAX_BUBBLE_DURATION,
+		true,
+		true,
+	).timeout.connect(queue_free)
 		
 func boundary_collision():
 	queue_free()
--- constants.gd
+++ constants.gd
@@ -1,3 +1,4 @@
 class_name Constants extends Node
 
 static var MAX_BUBBLE_SCALE = 0.9999
+static var MAX_BUBBLE_DURATION = 25
