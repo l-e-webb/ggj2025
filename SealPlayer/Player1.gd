6b246ceb09920d2a09381aad6008137bc7a3f0f7
Louis Webb <louis.webb5@gmail.com>
2025-01-25 16:51:53 -0400

    Add character double jumping

--- SealPlayer/Player1.gd
+++ SealPlayer/Player1.gd
@@ -1,25 +1,57 @@
 extends CharacterBody2D
 
+var time_since_last_jump_command = 10000. # just a big number
+var time_since_on_floor = 10000. # just a big number
+var has_air_jump = false
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+func _ready():
+	SignalBus.set_player_position.connect(_on_set_player_position)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if is_on_floor():
+		time_since_on_floor = 0.
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		time_since_on_floor += delta
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		time_since_last_jump_command = 0.
+	else:
+		time_since_last_jump_command += delta
+	
+	# Add the gravity.
+	velocity += get_gravity() * delta
+
+	handle_jump()
+	handle_horizontal_motion()
 
 	move_and_slide()
+
+func handle_jump():
+	if time_since_last_jump_command > Constants.PLAYER_JUMP_BUFFER_TIME:
+		# The player has not hit jump recently
+		
+		return
+
+	# Commence jumping
+	if time_since_on_floor < Constants.PLAYER_JUMP_BUFFER_TIME:
+		# Floor jump
+		velocity.y = -Constants.PLAYER_FLOOR_JUMP_VELOCITY
+		has_air_jump = true
+	elif has_air_jump and time_since_on_floor > Constants.MIN_TIME_BEFORE_SECOND_JUMP:
+		# Air jump
+		velocity.y = -Constants.PLAYER_AIR_JUMP_VELOCITY
+		has_air_jump = false
+	else:
+		# Attempting to jump in the air with no air jump remaining, or too soon
+		# after a ground jump
+		pass
+
+func handle_horizontal_motion():
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * Constants.PLAYER_HORIZONTAL_SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, Constants.PLAYER_HORIZONTAL_SPEED)
+
+func _on_set_player_position(pos: Vector2):
+	global_position = pos
