341d481fb89d0f42f7e40b72f6cd37ab99cc9765
efraser5005 <efraser5005@users.noreply.github.com>
2025-01-25 16:15:50 -0400

    Art phase i (#8)
    * background art
    
    * platforms, bubble and wand
    
    * bubble wand sprites and cooldown
    
    * refactor cooldown

--- bubble_wand/bubble_wand_controller.gd
+++ bubble_wand/bubble_wand_controller.gd
@@ -3,6 +3,10 @@ extends Node2D
 const PLAIN_BUBBLE = preload("res://bubbles/PlainBubble.tscn")
 
 var bubble = null
+var can_bubble = true
+
+var wand_with_bubble = preload("res://bubble_wand/bubblewandwithbubble.png")
+var wand_empty = preload("res://bubble_wand/bubblewand.png")
 
 func _process(delta: float) -> void:
 	# movement
@@ -26,19 +30,40 @@ func _input(event: InputEvent) -> void:
 			
 		
 func blow_bubble():
-	bubble = PLAIN_BUBBLE.instantiate()
-	add_child(bubble)
-	bubble.scale = Vector2(1, 1) * Constants.BUBBLE_MIN_SCALE
+	print("blow_bubble says: " + str(can_bubble))
+	if can_bubble:
+		bubble = PLAIN_BUBBLE.instantiate()
+		add_child(bubble)
+		bubble.scale = Vector2(1, 1) * Constants.BUBBLE_MIN_SCALE
 	
 	
 func release_bubble():
 	bubble.is_floating = true
 	var bubble_global_pos = bubble.global_position
+	$Sprite2D.set_texture(wand_empty)
 	remove_child(bubble)
+	
+	can_bubble = false
+	print("release_bubble_pre_cooldown: " + str(can_bubble))
+	cooldown()
 	
 	get_tree().root.add_child(bubble)
 	bubble.global_position = bubble_global_pos
 	bubble = null
+	
+func cooldown():
+	print("cooldown start: " + str(can_bubble))
+	get_tree().create_timer(
+		Constants.BUBBLE_WAND_COOLDOWN,
+		true,
+		true,
+	).timeout.connect(reload)
+	
+	
+func reload():
+	$Sprite2D.set_texture(wand_with_bubble)
+	can_bubble = true
+	print("reload: " + str(can_bubble))
 	
 
 # stretch: change bubble type
