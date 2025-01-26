cf935c2f474c6d041223ff8a30b9ae705d039ba3
Louis Webb <louis.webb5@gmail.com>
2025-01-26 11:37:41 -0400

    Add level advancement

--- game_controller.gd
+++ game_controller.gd
@@ -1,16 +1,32 @@
 extends Node2D
 
-@export var starting_level: String
+@export var level_order: Array = []
+
 var current_level = null
+var current_level_name: String
 
 func _ready():
 	SignalBus.load_level.connect(load_level)
-	if starting_level:
-		SignalBus.load_level.emit(starting_level)
+	SignalBus.player_win.connect(_on_win)
+	if !level_order.is_empty():
+		SignalBus.load_level.emit(level_order[0])
+	
 
 func load_level(name: String):
 	if current_level != null:
 		current_level.queue_free()
+	current_level_name = name
 	var full_name = "res://levels/%s.tscn" % name
 	current_level = load(full_name).instantiate()
 	add_child(current_level)
+
+func _on_win():
+	await get_tree().create_timer(0.5).timeout
+	var current_level_index = level_order.find(current_level_name)
+	if current_level_index == -1:
+		return
+	
+	current_level_index += 1
+	current_level_index = current_level_index % level_order.size()
+	if current_level_index < level_order.size():
+		SignalBus.load_level.emit(level_order[current_level_index])
